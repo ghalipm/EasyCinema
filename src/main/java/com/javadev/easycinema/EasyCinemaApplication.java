a99dfc9ca11907b9aabb9caa9d0f5f2f6319328a
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-07-18 22:29:18 -0400

    Initial commit

---
+++ src/main/java/com/javadev/easycinema/EasyCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.javadev.easycinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCinemaApplication.class, args);
+    }
+
+}
